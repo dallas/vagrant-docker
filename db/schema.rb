56897a93aed12e8e59ad589cee20481017c3c66a
dallas <code@dallasreedy.com>
2015-04-12 01:18:36 -0600

    got a different schema.rb somehow

--- db/schema.rb
+++ db/schema.rb
@@ -16,14 +16,14 @@ ActiveRecord::Schema.define(version: 20150408211656) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "widgets", force: true do |t|
+  create_table "widgets", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
